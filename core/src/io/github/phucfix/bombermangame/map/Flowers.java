a18ab78f88568ac91a56893311bb728066f20605
phuc50centimeters <186391026+phuc50cm@users.noreply.github.com.>
2025-03-27 02:38:36 +0700

    Initial Commit - Add Libgdx Project setup. - Add resource. - Implement simple GameMap with player and MenuScreen

---
+++ core/src/io/github/phucfix/bombermangame/map/Flowers.java
@@ -0,0 +1,36 @@
+package io.github.phucfix.bombermangame.map;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import io.github.phucfix.bombermangame.texture.Drawable;
+import io.github.phucfix.bombermangame.texture.Textures;
+
+/**
+ * Flowers are a static object without any special properties.
+ * They do not have a hitbox, so the player does not collide with them.
+ * They are purely decorative and serve as a nice floor decoration.
+ */
+public class Flowers implements Drawable {
+    
+    private final int x;
+    private final int y;
+    
+    public Flowers(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    @Override
+    public TextureRegion getCurrentAppearance() {
+        return Textures.FLOWERS;
+    }
+    
+    @Override
+    public float getX() {
+        return x;
+    }
+    
+    @Override
+    public float getY() {
+        return y;
+    }
+}
---
+++ core/src/io/github/phucfix/bombermangame/texture/Drawable.java
@@ -0,0 +1,38 @@
+package io.github.phucfix.bombermangame.texture;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import io.github.phucfix.bombermangame.screen.GameScreen;
+
+/**
+ * Represents something that can be drawn on the screen.
+ * NOTE: The position returned by {@link #getX()} and {@link #getY()} is the
+ * position of the BOTTOM LEFT CORNER of the texture in the game.
+ */
+public interface Drawable {
+    
+    /**
+     * Gets the current appearance of the Drawable.
+     * This can change over time.
+     * @return The current appearance as a {@link TextureRegion}.
+     */
+    TextureRegion getCurrentAppearance();
+    
+    /**
+     * Gets the X coordinate of the drawable in the game world grid.
+     * Note that this is a TILE coordinate, not a pixel coordinate.
+     * It must be multiplied by {@link GameScreen#TILE_SIZE_PX}
+     * and {@link GameScreen#SCALE} to get the pixel coordinate.
+     * @return The X coordinate of the drawable.
+     */
+    float getX();
+    
+    /**
+     * Gets the Y coordinate of the drawable in the game world grid.
+     * Note that this is a TILE coordinate, not a pixel coordinate.
+     * It must be multiplied by {@link GameScreen#TILE_SIZE_PX}
+     * and {@link GameScreen#SCALE} to get the pixel coordinate.
+     * @return The Y coordinate of the drawable.
+     */
+    float getY();
+    
+}
